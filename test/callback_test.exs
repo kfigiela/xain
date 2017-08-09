a039560baca7edd637017a97ee8dd031d91a7b50
Miron Markowski <majron15@gmail.com>
2017-08-09 11:56:42 +0200

    Fix callback test

--- test/callback_test.exs
+++ test/callback_test.exs
@@ -2,14 +2,14 @@ defmodule Xain.CallbackTest do
   use ExUnit.Case
   use Xain
 
-  def callback(output), do: {:safe, output}
+  def callback(output), do: output |> Phoenix.HTML.safe_to_string()
 
   test "after callback" do
     Application.put_env :xain, :after_callback, {__MODULE__, :callback}
     result = markup safe: true do
-      div
+      div()
     end
     Application.put_env :xain, :after_callback, nil
-    assert result == {:safe, "<div></div>"}
+    assert result == "<div></div>"
   end
 end
